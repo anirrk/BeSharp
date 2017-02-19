033b5cb0bde8d67471f5a2fc4bb85ec063b6084d
Anirudh Kaushik <anirudhkaushik@Anirudhs-MacBook-Pro.local>
2017-02-19 06:42:37 -0800

    uploads work

--- njtw/nodeJsToWatson.js
+++ njtw/nodeJsToWatson.js
@@ -1,89 +1,108 @@
 
-var express = require('express')
-var    multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var express = require('express'),
+    multer  = require('multer')
 
 var app = express()
+app.use(multer({ dest: './uploads/'}))
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-// app.post('/upload', upload, function(req, res){
-//     console.log(req.body) // form fields
-//     console.log(req.files) // form files
-//     res.status(204).end()
-// });
-
-// app.post('/upload', upload.array(), function (req, res, next) {
-//   // req.body contains the text fields
-// })
-
-
-
-app.post('/upload',function(req,res){
-    console.log('FIRST TEST: ' + JSON.stringify(req.files));
-    console.log('second TEST: ' +req.files.theFile.name);
-    fs.readFile(req.files.theFile.path, function (err, data) {
-        var newPath = "/home/path/to/your/directory/"+req.files.theFile.name;
-        fs.writeFile(newPath, data, function (err) {
-          res.send("hi");  
-        });
-    });
+app.post('/upload', function(req, res){
+    console.log(req.body) // form fields
+    console.log(req.files) // form files
+    // res.status(204).end()
+    res.send("upload worked");  
 });
-
-// app.post('/upload', function (req, res) {
-// 	// console.log(req);
-// 	// console.log(res);
-// 	// console.log(req.files);
-// 	console.log(req.body);
-
-// 	    var tempPath = req.files.file.path,
-// 	        targetPath = path.resolve('./uploads/image.png');
-// 	    if (path.extname(req.files.file.name).toLowerCase() === '.png') {
-// 	        fs.rename(tempPath, targetPath, function(err) {
-// 	            if (err) throw err;
-// 	            console.log("Upload completed!");
-// 	        });
-// 	    } else {
-// 	        fs.unlink(tempPath, function () {
-// 	            if (err) throw err;
-// 	            console.error("Only .png files are allowed!");
-// 	        });
-// 	    }
-// 	    // ...
-// 	});
-
 
 app.listen(3000);
 
-// express.get('/', function (req, res) {
-//   'use strict';
+// var express = require('express')
+// var    multer  = require('multer')
+// var upload = multer({ dest: 'uploads/' })
 
-// 	const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-// 	const fs = require('fs');
+// var app = express()
 
-// 	const visual_recognition = new VisualRecognitionV3({
-// 	  api_key: '441a7ba7a58fd8edf5bfa0b84e62455da5727ce3',
-// 	  version_date: '2016-05-19'
-// 	});
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
 
-// 	const params = {
-// 	  // must be a .zip file containing images
-// 	  images_file: fs.createReadStream('./selfie.JPG'),
-// 	  classifier_ids : ["fruit_1999080847"],
-// 	  threshold : 0.0
-// 	};
+// // app.post('/upload', upload, function(req, res){
+// //     console.log(req.body) // form fields
+// //     console.log(req.files) // form files
+// //     res.status(204).end()
+// // });
 
-// 	visual_recognition.classify(params, function(err, responds) {
-// 	  if (err) {
-// 	    res.send(err);
-// 	  } else {
-// 	    res.send(responds);
-// 	  }
-// 	});
-// })
+// // app.post('/upload', upload.array(), function (req, res, next) {
+// //   // req.body contains the text fields
+// // })
 
-// // app.use(express.bodyParser({uploadDir:'.'}));
-// app.listen(3000)
+
+
+// app.post('/upload',function(req,res){
+//     console.log('FIRST TEST: ' + JSON.stringify(req.files));
+//     console.log('second TEST: ' +req.files.theFile.name);
+//     fs.readFile(req.files.theFile.path, function (err, data) {
+//         var newPath = "/home/path/to/your/directory/"+req.files.theFile.name;
+//         fs.writeFile(newPath, data, function (err) {
+//           res.send("hi");  
+//         });
+//     });
+// });
+
+// // app.post('/upload', function (req, res) {
+// // 	// console.log(req);
+// // 	// console.log(res);
+// // 	// console.log(req.files);
+// // 	console.log(req.body);
+
+// // 	    var tempPath = req.files.file.path,
+// // 	        targetPath = path.resolve('./uploads/image.png');
+// // 	    if (path.extname(req.files.file.name).toLowerCase() === '.png') {
+// // 	        fs.rename(tempPath, targetPath, function(err) {
+// // 	            if (err) throw err;
+// // 	            console.log("Upload completed!");
+// // 	        });
+// // 	    } else {
+// // 	        fs.unlink(tempPath, function () {
+// // 	            if (err) throw err;
+// // 	            console.error("Only .png files are allowed!");
+// // 	        });
+// // 	    }
+// // 	    // ...
+// // 	});
+
+
+// app.listen(3000);
+
+// // express.get('/', function (req, res) {
+// //   'use strict';
+
+// // 	const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+// // 	const fs = require('fs');
+
+// // 	const visual_recognition = new VisualRecognitionV3({
+// // 	  api_key: '441a7ba7a58fd8edf5bfa0b84e62455da5727ce3',
+// // 	  version_date: '2016-05-19'
+// // 	});
+
+// // 	const params = {
+// // 	  // must be a .zip file containing images
+// // 	  images_file: fs.createReadStream('./selfie.JPG'),
+// // 	  classifier_ids : ["fruit_1999080847"],
+// // 	  threshold : 0.0
+// // 	};
+
+// // 	visual_recognition.classify(params, function(err, responds) {
+// // 	  if (err) {
+// // 	    res.send(err);
+// // 	  } else {
+// // 	    res.send(responds);
+// // 	  }
+// // 	});
+// // })
+
+// // // app.use(express.bodyParser({uploadDir:'.'}));
+// // app.listen(3000)
 
